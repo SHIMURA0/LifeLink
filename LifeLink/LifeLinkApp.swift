ea6a2083e7f3e712dacec2931c3d19ad401e8670
Honglie Zhang <hongliez27@gmail.com>
2024-10-09 14:41:55 +0800

    Initial Commit

---
+++ LifeLink/LifeLinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  LifeLinkApp.swift
+//  LifeLink
+//
+//  Created by Shimura on 2024/10/9.
+//
+
+import SwiftUI
+
+@main
+struct LifeLinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LifeLinkTests/LifeLinkTests.swift
@@ -0,0 +1,17 @@
+//
+//  LifeLinkTests.swift
+//  LifeLinkTests
+//
+//  Created by Shimura on 2024/10/9.
+//
+
+import Testing
+@testable import LifeLink
+
+struct LifeLinkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
